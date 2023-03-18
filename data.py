1c4828132ccb20b7f1de3cd98299a71a7c392c23
Jiawen9 <86347240+Jiawen9@users.noreply.github.com>
2023-03-19 00:22:04 +0800

    2023-3-19
    2023-3-19

--- data.py
+++ data.py
@@ -12,7 +12,7 @@ import json
 class SourceDataDemo:
 
     def __init__(self):
-        self.title = '大数据可视化展板通用模板'
+        self.title = '智能方法性能边界对比分析原理验证系统'
         self.counter = {'name': '2018年总收入情况', 'value': 12581189}
         self.counter2 = {'name': '2018年总支出情况', 'value': 3912410}
         self.echart1_data = {
