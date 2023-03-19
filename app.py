09c7c658137283f4f46b0aa9ce161590b10b6b86
Jiawen9 <86347240+Jiawen9@users.noreply.github.com>
2023-03-19 17:48:36 +0800

    3-19-17
    3-19-17

--- app.py
+++ app.py
@@ -26,13 +26,8 @@ def home():
 
 @app.route('/getTableData')
 def getTableData():
-    cols = []
-    data = []
-    data.append({"id":1,"username":"user1","sex":"男"})
-    cols.append({"field":'id', "title": 'ID'})
-    cols.append({"field":'username', "title": '用户名'})
-    cols.append({"field":'sex', "title": '性别'})
-    return {"cols":cols,"data":data}
+    _, _, _ ,_, _, _, _, _,_, dbn_table = get_echart1_data()
+    return dbn_table
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', debug=False)
