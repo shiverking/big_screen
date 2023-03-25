17e44308acc46d4532898e0f6e67ab85ebbe2ea8
Jiawen9 <86347240+Jiawen9@users.noreply.github.com>
2023-03-25 19:46:01 +0800

    325

--- service.py
+++ service.py
@@ -4,12 +4,12 @@ import math
 
 def get_dbnModel_res():
     upper_left_corner, data_pie, time_sum, x, y, upper_right_corner, lower_right_corner = DBN_RES()
-    print(data_pie)
     time_line_dbn = [{'name':'模型预测时间-LSTM', 'type': 'line', 'data': y}]
     return upper_left_corner, data_pie, time_sum, x, time_line_dbn, upper_right_corner, lower_right_corner
 
 def get_lstmModel_res():
     bottom_left_corner, time_sum, x, y, upper_right_corner, lower_right_corner = LSTM_RES()
+    print(bottom_left_corner)
     time_line_lstm = [{'name':'模型预测时间-LSTM', 'type': 'line', 'data': y}]
     return bottom_left_corner, time_sum, x, time_line_lstm, upper_right_corner, lower_right_corner
 
