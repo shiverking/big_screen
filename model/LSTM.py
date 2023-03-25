ea562c3a8fa98fcf0fb80ab399c39d5f64a9d055
Jiawen9 <86347240+Jiawen9@users.noreply.github.com>
2023-03-25 18:33:05 +0800

    3-25大改
    3-25大改

--- model/LSTM.py
+++ model/LSTM.py
@@ -10,8 +10,6 @@
 import numpy as np
 import pandas as pd
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
 import time
 import math
 import matplotlib.pyplot as plt
@@ -19,7 +17,7 @@ from sklearn.preprocessing import *
 from torch.utils.data import DataLoader, TensorDataset
 from sklearn.metrics import classification_report
 from model.DBN import DataProcessing
-import warnings
+import warnings  # 解决报错：Precision and F-score are ill-defined with no predicted samples.
 
 warnings.filterwarnings("ignore")
 
@@ -31,20 +29,20 @@ layer_size = 1  # 模型的层数
 output_size = 8  # 意图的个数
 batch_size_train = 32
 batch_size_test = 1
-epochs = 300  # 训练轮数
-device = torch.device("cuda:0" if torch.cuda.is_available() else 'cpu')
+epochs = 500  # 训练轮数
+device = torch.device("cuda:0" if torch.cuda.is_available() else 'cpu') #设备
 
 
-def data_rolling(x, seque_size):
+def data_rolling(o, seq_size):
     """
     generate the sequential data
-    :param x: original data
-    :param seque_size: time-step
+    :param o: original data
+    :param seq_size: time-step
     :return: time series data
     """
     x_Rolling = []
-    for i in range(x.shape[0] - seque_size):
-        x_new = x[i:i + seque_size, ]
+    for d in range(o.shape[0] - seq_size):
+        x_new = o[d:d + seq_size, ]
         x_Rolling.append(x_new)
     return np.array(x_Rolling)
 
@@ -72,14 +70,15 @@ def data_split(dataset, train_name, test_name, seque_size):
     return data_trainLoader, data_testLoader
 
 
-class LSTM(nn.Module):
+class LSTM(torch.nn.Module):
     def __init__(self, input_dim, hidden_dim, layer_dim, output_dim):
         super(LSTM, self).__init__()
         self.hidden_dim = hidden_dim
         self.layer_dim = layer_dim
-        self.lstm = nn.LSTM(input_dim, hidden_dim, layer_dim, batch_first=True)  # input.shape (batch, L, input_size)
+        self.lstm = torch.nn.LSTM(input_dim, hidden_dim, layer_dim,
+                                  batch_first=True)  # input.shape (batch, L, input_size)
         # 全连接层
-        self.linear = nn.Linear(hidden_dim, output_dim)
+        self.linear = torch.nn.Linear(hidden_dim, output_dim)
 
     def forward(self, input_data):
         #  h0,c0.shape (1, batch, hidden_size)   out.shape (L, batch, num_directions x hidden_size)
@@ -94,7 +93,7 @@ class LSTM(nn.Module):
 def accuracy_Train(train_loader, sequence_size, input_size, model, epochMax=30, learning_rate=0.1):
     timeTotal_start = time.time()
     # 损失函数
-    loss_function = nn.CrossEntropyLoss()
+    loss_function = torch.nn.CrossEntropyLoss()
     # 优化器
     optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
@@ -181,7 +180,7 @@ def accuracy_Test(test_loader, net, test_name, sequence_size, input_size):
         predict = torch.max(outputs.data, 1)[1]  # 获取预测概率最大值的下标
         y_predict.append(predict.cpu().numpy())  # 获取预测概率最大值的下标
         y_test.append(labelsT.cpu().numpy())  # 统计测试集的大小
-        prob = F.softmax(outputs.data, dim=1)  # dim = 0,在列上进行Softmax;dim=1,在行上进行Softmax
+        prob = torch.nn.functional.softmax(outputs.data, dim=1)  # dim = 0,在列上进行Softmax;dim=1,在行上进行Softmax
 
         for k in range(output_size):
             Intention[k].append((prob[0][k]).cpu().numpy())
@@ -190,32 +189,40 @@ def accuracy_Test(test_loader, net, test_name, sequence_size, input_size):
         t_list.append(time_end - time_start)
 
     Aver_testTime = np.average(t_list)
-    perfor_report = classification_report(y_test, y_predict)
+    perfor_report = classification_report(y_test, y_predict, output_dict=True)
 
     plt.figure()
-    '''陈嘉文加'''
     plot_print = list() 
     IntentionName = ['超高空盘旋', '高空盘旋', '中空高速巡航', '中空巡航', '中空盘旋', '低空高速盘旋', '低空盘旋', '低空高速巡航']
     for k in range(output_size):
-        plot_print.append({'name': IntentionName[k], 'data': [float(x) for x in Intention[k]]})
         plt.plot(range(len(y_predict)), Intention[k])
+        plot_print.append({'name': IntentionName[k], 'type': 'line', 'smooth': 'true', 'data': [float(x) for x in Intention[k]]})
     plt.legend(['超高空盘旋', '高空盘旋', '中空高速巡航', '中空巡航', '中空盘旋', '低空高速盘旋', '低空盘旋', '低空高速巡航'])
     plt.xlabel("Number of Step")
     plt.ylabel("Intention")
-    plt.title("model_test LSTM ({})".format(test_name))
+    plt.title("model_test LSTM ({}), time_step={}".format(test_name, sequence_size))
 
-    return Aver_testTime, perfor_report, target_num, predict_num, acc_num, plt, plot_print, IntentionName
+    return Aver_testTime, perfor_report, target_num, predict_num, acc_num, plt, plot_print
+
+
+def metric_compute(accNum, preNum, tarNum):
+    pre = (accNum / preNum * 100)
+    rec = (accNum / tarNum * 100)
+    F = 2 * rec * pre / (rec + pre)
+    pre = [0 if math.isnan(p) else p for p in pre.squeeze(0).numpy().tolist()]
+    rec = [0 if math.isnan(r) else r for r in rec.squeeze(0).numpy().tolist()]
+    F = [0 if math.isnan(f) else f for f in F.squeeze(0).numpy().tolist()]
+    return pre, rec, F
 
 
 def LSTM_RES():
-    time_start = time.time()  # 记录开始时间
     data = pd.read_excel('./data/正南打击利佩茨克机场数据/data_new.xlsx')  # 导入原始数据
     typeName = '战斗机'
-    sequence_size = 1
     data_proc = DataProcessing(data, typeName)  # 数据预处理
     data_coding = data_proc.data_Code  # 输入模型的数据
     trainName = 'F-22 科加尔尼西亚 #5'  # 输入训练目标
     testName = 'F-22 科加尔尼西亚 #6'  # 输入测试目标——【目标选择】
+    sequence_size = 1  # time_step
     trainData, testData = data_split(data_coding, trainName, testName, sequence_size)  # 训练/测试集划分
 
     n_feature = data_coding.iloc[:, 3:]
@@ -223,73 +230,159 @@ def LSTM_RES():
 
     model = LSTM(input_size, hidden_size, layer_size, output_size)
 
-    modelTr, iter_list, loss_list, acc_list = accuracy_Train(train_loader=trainData, sequence_size=1, input_size=input_size,\
-            model=model, epochMax=30, learning_rate=0.1)  # 模型训练结果
-    Aver_time, perform_report, target_num, predict_num, acc_num, plt, plot_print, legend = accuracy_Test(testData, modelTr, testName,
-                                                                                     sequence_size, input_size)  # 模型测试结果
-    # plt.show()    # 【可视化展示1】：各模型的意图识别结果图
+    # modelTr, iter_list, loss_list, acc_list = accuracy_Train(trainData, sequence_size, epochs, 0.01)  # 模型训练结果
+    # Aver_time, perform_report, target_num, predict_num, acc_num, plt = accuracy_Test(testData, modelTr, testName,
+    #                                                                                  sequence_size)  # 模型测试结果
+    # # plt.show()    # 【可视化展示1】：各模型的意图识别结果图
     # print("Average TestTime:{}".format(Aver_time))
-    time_end = time.time()  # 记录结束时间
-    time_sum = time_end - time_start 
-    # 【可视化展示2-2】：统计信息图（同DBN，展示一次就好）+性能指标值输出+各个模型预测精度对比图
-    print("perform_report:'\n' {}".format(perform_report))
+    #
+    # # 【可视化展示2-2】：统计信息图（同DBN，展示一次就好）+性能指标值输出+各个模型预测精度对比图
+    # print("perform_report:'\n' {}".format(perform_report))
+    #
+    # # 【可视化展示2-3】：统计信息图+性能指标值输出+各个模型预测精度对比图
+    # precision = (acc_num / predict_num * 100).squeeze(0).numpy().tolist()
+    # precision = [0 if math.isnan(x) else x for x in precision]
+    # IntentionName = ['超高空盘旋', '高空盘旋', '中空高速巡航', '中空巡航', '中空盘旋', '低空高速盘旋', '低空盘旋', '低空高速巡航']
+    #
+    # fig, ax = plt.subplots()
+    # ax.barh(IntentionName, precision, align='center', label='LSTM', color='orange')
+    # for a, b in zip(IntentionName, precision):
+    #     plt.text(b + 1, a, "%.2f%%" % b, ha='center', va='center', fontsize=8, color='k')
+    # ax.set_yticks(IntentionName)
+    # ax.set_xlabel('precision(%)')
+    # ax.set_title('各个模型预测精度对比图')
+    #
+    # # 【可视化展示3】：不同模型在不同冷启动时间下的准确率表
+    # sequence_size = [1, 5, 10, 15, 20]
+    # precision_list, accuracy_list, Aver_timeList = [], [], []
+    # for i in sequence_size:
+    #     trainData, testData = data_split(data_coding, trainName, testName, i)  # 训练/测试集划分
+    #
+    #     modelTr, iter_list, loss_list, acc_list = accuracy_Train(trainData, i, epochs, 0.01)  # 模型训练结果
+    #     Aver_time, perform_report, target_num, predict_num, acc_num, plt = accuracy_Test(testData, modelTr,
+    #                                                                                      testName, i)  # 模型测试结果
+    #     precision = (acc_num / predict_num * 100).squeeze(0).numpy().tolist()
+    #     precision_list.append(precision)       # 各类精确率
+    #     accuracy = (100. * acc_num.sum(1) / target_num.sum(1)).numpy()
+    #     accuracy_list.append(accuracy)         # 整体准确率
+    #
+    #     Aver_timeList.append(Aver_time)        # 预测时间
+    # precision_data = np.vstack((np.array(precision_list).T, np.array(accuracy_list).reshape(-1)))
+    #
+    # fig, ax = plt.subplots()
+    # IntentionName.append('整体')
+    # rowColours = ["#F0C9C0", "#F0C9C0", "#F0C9C0", "#F0C9C0", "#F0C9C0", "#F0C9C0", "#F0C9C0", "#F0C9C0", "#F2F2F2"]
+    # column_labels = ["LSTM(t=1)", "LSTM(t=5)", "LSTM(t=10)", "LSTM(t=15)", "LSTM(t=20)"]
+    # colColors = ["#00ccff"] * len(column_labels)
+    #
+    # ax.axis('off')       # 取消坐标轴
+    # ax.table(cellText=np.round(precision_data, 2), colLabels=column_labels, colColours=colColors, rowColours=rowColours,
+    #          rowLabels=IntentionName, cellLoc='center', rowLoc='center', loc="center")
+    # ax.set_title('不同模型在不同冷启动时间下的准确率表')
 
-    # 【可视化展示2-3】：统计信息图+性能指标值输出+各个模型预测精度对比图
-    precision = (acc_num / predict_num * 100).squeeze(0).numpy().tolist()
-    precision = [0 if math.isnan(x) else x for x in precision]
+    column_labels = ["time_step=1", "time_step=5", "time_step=10", "time_step=15", "time_step=20"]
     IntentionName = ['超高空盘旋', '高空盘旋', '中空高速巡航', '中空巡航', '中空盘旋', '低空高速盘旋', '低空盘旋', '低空高速巡航']
-    precision_lstm = precision
-    fig, ax = plt.subplots()
-    ax.barh(IntentionName, precision, align='center', label='LSTM', color='orange')
-    for a, b in zip(IntentionName, precision):
-        plt.text(b + 1, a, "%.2f%%" % b, ha='center', va='center', fontsize=8, color='k')
-    ax.set_yticks(IntentionName)
-    ax.set_xlabel('precision(%)')
-    ax.set_title('各个模型预测精度对比图')
-    # plt.show()
-
-    # 【可视化展示3】：不同模型在不同冷启动时间下的准确率表
     sequence_size = [1, 5, 10, 15, 20]
-    precision_list, accuracy_list, Aver_timeList = [], [], []
-    for i in sequence_size:
+    prec_list, rec_list, F_list, accur_list, wePre_list, weRec_list, weF_list, Aver_tList = [], [], [], [], [], [], [], []
+    bottom_left_corner = dict() ## 存放左下角折线图的list
+    for idx, i in enumerate(sequence_size):
+        if i == 1:
+            time_start = time.time()  # 记录开始时间
         trainData, testData = data_split(data_coding, trainName, testName, i)  # 训练/测试集划分
+        modelTr, iter_list, loss_list, acc_list = accuracy_Train(train_loader=trainData, sequence_size=i, input_size=input_size, model=model, epochMax=30, learning_rate=0.01)  # 模型训练结果
+        Aver_time, perform_report, target_num, predict_num, acc_num, plt, plot_print = accuracy_Test(testData, modelTr, testName, i, input_size)  # 模型测试结果
+        bottom_left_corner[column_labels[idx]] = dict()
+        bottom_left_corner[column_labels[idx]]['series'] = plot_print
+        bottom_left_corner[column_labels[idx]]['legend'] = IntentionName
+        bottom_left_corner[column_labels[idx]]['xAxis'] = {
+                                                            'type': 'category',
+                                                            'boundaryGap': 'false',
+                                                            'data': [x for x in range(200)]
+                                                        }
+        # 【可视化展示1——更新版本（左下角的图）】：LSTM模型意图识别结果图（之前只输出了t=1时的结果，现在跟可视化展示3一样，设置5个按钮，分别展示相应的内容）
 
-        modelTr, iter_list, loss_list, acc_list = accuracy_Train(train_loader=trainData, sequence_size=i, input_size=input_size,\
-            model=model, epochMax=30, learning_rate=0.1)  # 模型训练结果
-        Aver_time, perform_report, target_num, predict_num, acc_num, plt, _, _ = accuracy_Test(testData, modelTr,
-                                                                                         testName, i, input_size)  # 模型测试结果
-        precision = (acc_num / predict_num * 100).squeeze(0).numpy().tolist()
-        precision_list.append(precision)       # 各类精确率
-        accuracy = (100. * acc_num.sum(1) / target_num.sum(1)).numpy()
-        accuracy_list.append(accuracy)         # 整体准确率
+        accur_list.append(perform_report['accuracy'] * 100)
+        wePre_list.append(perform_report['weighted avg']['precision'] * 100)
+        weRec_list.append(perform_report['weighted avg']['recall'] * 100)
+        weF_list.append(perform_report['weighted avg']['f1-score'] * 100)
 
-        Aver_timeList.append(Aver_time)        # 预测时间
-    precision_data = np.vstack((np.array(precision_list).T, np.array(accuracy_list).reshape(-1)))
+        precision, recall, F1 = metric_compute(acc_num, predict_num, target_num)
+        prec_list.append(precision)  # 各类精确率
+        rec_list.append(recall)  # 各类召回率
+        F_list.append(F1)  # 各类F1
 
-    fig, ax = plt.subplots()
-    IntentionName.append('整体')
-    rowColours = ["#F0C9C0", "#F0C9C0", "#F0C9C0", "#F0C9C0", "#F0C9C0", "#F0C9C0", "#F0C9C0", "#F0C9C0", "#F2F2F2"]
-    column_labels = ["LSTM(t=1)", "LSTM(t=5)", "LSTM(t=10)", "LSTM(t=15)", "LSTM(t=20)"]
-    colColors = ["#00ccff"] * len(column_labels)
+        Aver_tList.append(Aver_time)  # 预测时间
+        if i == 1:
+            time_end = time.time()  # 记录结束时间
+            time_sum = time_end - time_start 
+    print(bottom_left_corner)        
+    # 【可视化展示——更新版本，展示在右上角】
+    upper_right_corner = dict() ## 右上角的条形图
+    weMetricName = ['accuracy', 'weighted precision', 'weighted recall', 'weighted f1-score']
+    fig, axes = plt.subplots(len(column_labels), 1, figsize=(11, 10))
+    plt.title('不同模型的整体性能指标分析', fontsize=8)
+    for i in range(len(column_labels)):
+        weMetric_list = [accur_list[i], wePre_list[i], weRec_list[i], weF_list[i]]
+        upper_right_corner[column_labels[i]] = dict()
+        upper_right_corner[column_labels[i]]['series'] = [
+            {   
+                'name': 'LSTM',
+                'data': weMetric_list,
+                'type': 'bar',
+            }
+        ]
+        upper_right_corner[column_labels[i]]['yAxis'] = {
+                                                            'type': 'category',
+                                                            'data': weMetricName
+                                                        }
+        axes[i].barh(weMetricName, weMetric_list, align='center', label='LSTM', color='orange')
+        for a, b in zip(weMetricName, weMetric_list):
+            axes[i].text(b + 1, a, "%.2f%%" % b, ha='center', va='center', fontsize=8, color='k')
+        axes[i].set_yticks(weMetricName)
+        axes[i].set_xlabel('percent(%)', fontsize=8)
+        axes[i].set_title('冷启动时间{}的整体性能指标值'.format(column_labels[i]), fontsize=8)
+    print(upper_right_corner)
 
-    ax.axis('off')       # 取消坐标轴
-    ax.table(cellText=np.round(precision_data, 2), colLabels=column_labels, colColours=colColors, rowColours=rowColours,
-             rowLabels=IntentionName, cellLoc='center', rowLoc='center', loc="center")
-    lstm_table = {'colLabels': column_labels, 'rowLabels' :IntentionName, 'data':np.round(precision_data, 2)}
-    ax.set_title('不同模型在不同冷启动时间下的准确率表')
-    # plt.show()
-
+    # 【可视化展示3——更新版本（原先可视化展示2-3和可视化展示3的结合），展示在右下角】
+    lower_right_corner = dict() ## 右下角条形图
+    metricName = ['precision', 'recall', 'f1-score']
+    metric_list = [prec_list, rec_list, F_list]
+    for j in range(len(metricName)):
+        lower_right_corner[metricName[j]] = dict()
+        fig, axes = plt.subplots(len(column_labels), 1, figsize=(11, 10))
+        plt.title('不同模型的各类别性能指标分析-{}'.format(metricName[j]), fontsize=8)
+        for i in range(len(column_labels)): 
+            lower_right_corner[metricName[j]][column_labels[i]] = dict()
+            lower_right_corner[metricName[j]][column_labels[i]]['series'] = [
+                {
+                    'name': 'LSTM',
+                    'data': metric_list[j][i],
+                    'type': 'bar',
+                }
+            ]
+            lower_right_corner[metricName[j]][column_labels[i]]['yAxis'] = {
+                                                                                'type': 'category',
+                                                                                'data': IntentionName
+                                                                            }
+            axes[i].barh(IntentionName, metric_list[j][i], align='center', label='LSTM', color='orange')
+            for a, b in zip(IntentionName, metric_list[j][i]):
+                axes[i].text(b + 1, a, "%.2f%%" % b, ha='center', va='center', fontsize=8, color='k')
+            axes[i].set_yticks(IntentionName)
+            axes[i].set_xlabel('percent(%)', fontsize=8)
+            axes[i].set_title('冷启动时间{}的各类别性能指标值-{}'.format(column_labels[i], metricName[j]), fontsize=8)
+    print(lower_right_corner)
     # 【可视化展示4】：运行时间
     plt.figure()
     x = ['time_step=1', 'time_step=5', 'time_step=10', 'time_step=15', 'time_step=20']
-    y = Aver_timeList
+    y = Aver_tList
 
     plt.plot(x, y, color='k', linewidth=1, marker='o', markersize=5, label="预测时间")
     for a, b in zip(x, y):
         plt.text(a, b, '%.4f' % b, ha='center', va='bottom', fontsize=11, color='k')
     plt.title("各模型的预测时间", color='k')
     # plt.show()
-    return plot_print, legend, time_sum, x, y, precision_lstm, lstm_table
+
+    return bottom_left_corner, time_sum, x, y, upper_right_corner, lower_right_corner
 
 if __name__ == '__main__':
     LSTM_RES()
